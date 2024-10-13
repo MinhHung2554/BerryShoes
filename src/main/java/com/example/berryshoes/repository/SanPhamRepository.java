b67c7e399704a53454c9664d9ee98be15142983d
MinhHung2554 <kendyhungst@gmail.com>
2024-10-13 16:00:09 +0700

    build repo

--- src/main/java/com/example/berryshoes/repository/SanPhamRepository.java
+++ src/main/java/com/example/berryshoes/repository/SanPhamRepository.java
@@ -1,4 +1,9 @@
 package com.example.berryshoes.repository;
 
-public class SanPhamRepository {
+import com.example.berryshoes.entity.SanPham;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SanPhamRepository extends JpaRepository<SanPham, Integer> {
 }
