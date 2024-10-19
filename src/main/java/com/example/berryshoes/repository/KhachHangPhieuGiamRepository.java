07ac157f5a8c290c6c75d8db05cf40f864ffb17e
DongThanhNguyen <dongtuber@gmail.com>
2024-10-19 17:27:00 +0700

    ok

--- src/main/java/com/example/berryshoes/repository/KhachHangPhieuGiamRepository.java
+++ src/main/java/com/example/berryshoes/repository/KhachHangPhieuGiamRepository.java
@@ -4,6 +4,9 @@ import com.example.berryshoes.entity.KhachHangPhieuGiam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KhachHangPhieuGiamRepository extends JpaRepository<KhachHangPhieuGiam, Integer> {
+
 }
