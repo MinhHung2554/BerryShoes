c1baa850a5a5e39be2528685572114f69a710088
MinhHung2554 <kendyhungst@gmail.com>
2024-10-19 14:23:01 +0700

    -.-

--- src/main/java/com/example/berryshoes/repository/PhuongThucThanhToanRepository.java
+++ src/main/java/com/example/berryshoes/repository/PhuongThucThanhToanRepository.java
@@ -2,8 +2,14 @@ package com.example.berryshoes.repository;
 
 import com.example.berryshoes.entity.PhuongThucThanhToan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PhuongThucThanhToanRepository extends JpaRepository<PhuongThucThanhToan, Integer> {
+
+//    @Query("SELECT p FROM PhuongThucThanhToan p WHERE p.idHoaDon = :idHoaDon")
+//    List<PhuongThucThanhToan> findByIdHoaDon(Integer idHoaDon);
 }
