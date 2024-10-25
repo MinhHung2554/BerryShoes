747ed2b701658d3cd86878e71d82a927f637382c
DongThanhNguyen <dongtuber@gmail.com>
2024-10-25 11:01:52 +0700

    CRUD sanpham,sanphamchitiet,phuongthucthanhtoan

--- src/main/java/com/example/berryshoes/dto/request/SanPhamChiTietRequest.java
+++ src/main/java/com/example/berryshoes/dto/request/SanPhamChiTietRequest.java
@@ -1,4 +1,19 @@
 package com.example.berryshoes.dto.request;
 
+import lombok.Data;
+
+@Data
 public class SanPhamChiTietRequest {
+    private String maSanPhamChiTiet;
+    private String qrCode;
+    private Integer soLuong;
+    private Double giaTien;
+    private String moTa;
+    private Integer idSanPham; // ID của sản phẩm
+    private Integer idKichCo; // ID của kích cỡ
+    private Integer idMauSac; // ID của màu sắc
+    private Integer idDotGiamGia; // ID của đợt giảm giá (nếu có)
+    private String nguoiTao;
+    private String nguoiCapNhat;
+    private Boolean trangThai = true;
 }
