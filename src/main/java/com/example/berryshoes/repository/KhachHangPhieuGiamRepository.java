6ffb22b1c58ff6de1171a54d85c0b0805f2fa8b5
MinhHung2554 <kendyhungst@gmail.com>
2024-11-08 09:19:23 +0700

    repo khpg

--- src/main/java/com/example/berryshoes/repository/KhachHangPhieuGiamRepository.java
+++ src/main/java/com/example/berryshoes/repository/KhachHangPhieuGiamRepository.java
@@ -1,12 +1,19 @@
 package com.example.berryshoes.repository;
 
+import com.example.berryshoes.entity.KhachHang;
 import com.example.berryshoes.entity.KhachHangPhieuGiam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface KhachHangPhieuGiamRepository extends JpaRepository<KhachHangPhieuGiam, Integer> {
+    @Query("SELECT s FROM KhachHangPhieuGiam s WHERE s.phieuGiamGia.id =:IdPhieu")
+    List<KhachHangPhieuGiam> findKhachHangPhieuGiamByIdPhieugiamgia(Integer IdPhieu);
+    @Query("SELECT s FROM KhachHangPhieuGiam s WHERE s.khachHang.id =:IdKH")
+    List<KhachHangPhieuGiam> findKhachHangPhieuGiamByIdKhachHang(Integer IdKH);
 
+    List<KhachHangPhieuGiam> findAllByKhachHang(KhachHang kh);
 }
