65fe07cb302a09f0517bbf06fe8b43c4b0b955f1
MinhHung2554 <kendyhungst@gmail.com>
2024-12-01 20:10:00 +0700

    fix kh bhtq

--- src/main/java/com/example/berryshoes/dto/request/BanTaiQuayRequest.java
+++ src/main/java/com/example/berryshoes/dto/request/BanTaiQuayRequest.java
@@ -2,7 +2,6 @@ package com.example.berryshoes.dto.request;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
