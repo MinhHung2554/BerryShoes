e2ea04d505060d35fda16ef0d757240ab6ff9562
MinhHung2554 <kendyhungst@gmail.com>
2024-12-01 20:33:21 +0700

    shipp

--- src/main/java/com/example/berryshoes/controller/ShippingController.java
+++ src/main/java/com/example/berryshoes/controller/ShippingController.java
@@ -1,14 +1,15 @@
 package com.example.berryshoes.controller;
 
 
+import com.example.berryshoes.entity.GioHang;
 import com.example.berryshoes.ghn.GhnClient;
+import com.example.berryshoes.repository.GioHangRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,10 +19,18 @@ public class ShippingController {
     @Autowired
     private GhnClient ghnClient;
 
+    @Autowired
+    private GioHangRepository gioHangRepository;
 
-    @GetMapping("/public/calculate-shipping-fee")
-    public Map<String, Object> calculateShippingFee(@RequestParam Integer toDistrictId, @RequestParam String toWardCode) {
-        return ghnClient.calculateShippingFee(toDistrictId,toWardCode, 3000);
+    @PostMapping("/public/calculate-shipping-fee")
+    public Map<String, Object> calculateShippingFee(@RequestParam Integer toDistrictId, @RequestParam String toWardCode, @RequestBody List<Integer> idGioHang) {
+        List<GioHang> list = gioHangRepository.findAllById(idGioHang);
+        Float kl = 0f;
+        for(GioHang g : list){
+            kl += 0.5f * g.getSoLuong();
+        }
+        Integer khoiLuong = kl.intValue() + 1;
+        return ghnClient.calculateShippingFee(toDistrictId,toWardCode, khoiLuong);
     }
 
     @GetMapping("/public/province")
