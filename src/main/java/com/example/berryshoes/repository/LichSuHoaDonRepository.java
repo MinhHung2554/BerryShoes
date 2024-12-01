e0e469c07299187a7b38feee8ac96a9d4cc88c8d
MinhHung2554 <kendyhungst@gmail.com>
2024-12-01 20:26:15 +0700

    lichsuhoadon

--- src/main/java/com/example/berryshoes/repository/LichSuHoaDonRepository.java
+++ src/main/java/com/example/berryshoes/repository/LichSuHoaDonRepository.java
@@ -10,8 +10,9 @@ import java.util.List;
 
 @Repository
 public interface LichSuHoaDonRepository extends JpaRepository<LichSuHoaDon, Integer> {
+
     List<LichSuHoaDon> findAllByHoaDonOrderByNgayTao(HoaDon hd);
 
     @Query("SELECT h FROM LichSuHoaDon h WHERE h.hoaDon.id =?1")
-    LichSuHoaDon findLichSuHoaDonByIdHoaDon(Integer idHoaDon);
+    List<LichSuHoaDon> findLichSuHoaDonByIdHoaDon(Integer idHoaDon);
 }
